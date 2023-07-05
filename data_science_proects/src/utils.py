b1ca14877183e44fe5650748068af110022663f4
Mayank Tripathi <mayanktripathi4u@gmail.com>
2023-07-05 01:09:56 -0500

    Model Trainer with hyperparameter tuning

--- data_science_proects/src/utils.py
+++ data_science_proects/src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,13 +19,25 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+
+            # added as a part of hyperparameter tuning -- Start
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # added as a part of hyperparameter tuning -- End
+
+            # model.fit(X_train, y_train) # commented after enabling hyperparameter tuning
 
             y_train_pred = model.predict(X_train)
 
